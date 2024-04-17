0b205378770d54dda6e95634e944d98f94fa7800
gustaavobraga <gustavobraga@pop-os.localdomain>
2024-04-17 06:31:58 -0300

    First commit

---
+++ R/soma.R
@@ -0,0 +1,14 @@
+#' Função para somar
+#'
+#' Essa função serve para somar dois números
+#'
+#' @param x um número
+#' @param y outro número
+#'
+#' @example
+#' soma(2,3)
+
+#' @export
+soma <- function(x, y){
+  x + y
+}
